76c7c21f843f45d00139ad8b1b85a27affe08388
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-06-09 10:43:19 +0200

    fixed code

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./style/style.css">
     <title>HOTEL HOMEPAGE</title>
 </head>
 <body>
@@ -50,7 +49,7 @@
         ];
 
         $parking_filter = ($_GET['if_parking'] ?? 'off');
-        $min_vote = $_GET['min_vote'];
+        $min_vote = intval($_GET['min_vote']); 
 
         $filtered_hotels = [];
         foreach($hotels as $hotel){
@@ -61,7 +60,7 @@
         if($parking_filter == 'on'){
             foreach($filtered_hotels as $i => $hotel){
                 if(!$hotel['parking']){
-                    array_splice($filtered_hotels, $i, 1);
+                    array_splice($filtered_hotels, $i, $i);
                 }
             }
         }
